9fefd1dfa81c69db6f1c419816576269027b2e57
Alvaro Jose Cardozo <137445907+Alvaro-C98@users.noreply.github.com>
2023-07-13 16:06:12 -0300

    MiPrimerPrograma

---
+++ MiPrimerPrograma.java
@@ -0,0 +1,7 @@
+public class MiPrimerPrograma{
+    public static void main(String [] args){
+        System.out.println("Mi nombre es Alvaro Cardozo");
+        System.out.println("Tengo 24 anios de edad");
+        System.out.println("Mi ciudad es Salta, Capital");
+    }
+}
